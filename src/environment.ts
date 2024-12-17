b5818a9980d37a2415e3d3f38fbb2d0e41e05b90
Anukul Pandey <anukul.030601@gmail.com>
2024-12-17 14:52:45 +0530

    feat: undo dev env

--- src/environment.ts
+++ src/environment.ts
@@ -2,8 +2,8 @@ import { AvailableNetworks, availableNetworks } from "@reef-defi/react-lib";
 
 let backendDomainName = 'reefscan.info';
 export const getNetworkCrowdsaleUrl = (networkName: AvailableNetworks)=>{
-  // return `https://ido-backend-${networkName}.${backendDomainName}/crowdsale`;
-  return `http://127.0.0.1:3000/crowdsale`;
+  return `https://ido-backend-${networkName}.${backendDomainName}/crowdsale`;
+  // return `http://127.0.0.1:3000/crowdsale`;
 }
 
 export const appAvailableNetworks = [
