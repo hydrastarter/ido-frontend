8c7b8c90e95793e9e18a89199c470f671df0f12a
Anukul Pandey <anukul.030601@gmail.com>
2025-03-26 13:39:01 +0530

    feat: new contracts

--- src/environment.ts
+++ src/environment.ts
@@ -3,7 +3,6 @@ import { AvailableNetworks, availableNetworks } from "@reef-defi/react-lib";
 let backendDomainName = 'reefscan.info';
 export const getNetworkCrowdsaleUrl = (networkName: AvailableNetworks)=>{
   return `https://ido-backend-${networkName}.${backendDomainName}/crowdsale`;
-  // return `http://127.0.0.1:3000/crowdsale`;
 }
 
 export const appAvailableNetworks = [
@@ -18,8 +17,8 @@ export const infuraSubDomainBaseUrl = process.env.REACT_APP_INFURA_SUBDOMAIN_LIN
 
 export const networkConfig={
   mainnet:{
-    LAUNCHPAD_FACTORY_ADDRESS: "0xcBA15dCb42a4F8Fee5A24e2B4fBdCbD0Df843Eb4",
-    PROXY_CONTRACT_MULTIOWNER: "0x31e8EfCB7D6816d9C0A7cdDcc94390aF9548ab6D"
+    LAUNCHPAD_FACTORY_ADDRESS: "0xd28817b6Af37E635575b0D579DbB042A4331b9Bb",
+    PROXY_CONTRACT_MULTIOWNER: "0xAcC9917E71Bb1e27eC98fc7b8D223882A37aAA32"
   },
   testnet:{
     LAUNCHPAD_FACTORY_ADDRESS: "0xf401b3Ec2298B80547B53dEEB8906677102A7F4B",
