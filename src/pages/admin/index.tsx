dd178965db7de6fd8f09e943992c0ed93bf78a2d
Anukul Pandey <62092256+anukulpandey@users.noreply.github.com>
2025-04-04 14:31:39 +0530

    fix: extrinsic url (#5)

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -922,6 +922,50 @@ export const Admin: React.FC = () => {
     buildFinalForm
   ]
 
+const endpoint = 'https://squid.subsquid.io/reef-explorer/graphql';
+
+async function fetchBlockHeight() {
+  try {
+    const query = `
+    query FetchBlockHeight {
+      transfers(limit: 1, where: {AND: {extrinsicHash_eq: "${txHash}"}}, orderBy: blockHeight_DESC) {
+        extrinsicId
+      }
+    }
+    `;
+
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: query,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (data.errors) {
+      console.error('Error fetching data:', data.errors);
+      return null;
+    }
+
+    const extrinsicId = data.data.transfers[0]?.extrinsicId;
+
+    if (extrinsicId) {
+      const blockHeightHex = extrinsicId.split("-")[1]
+      return blockHeightHex;
+    } else {
+      console.error('No blockHeight data found');
+      return null;
+    }
+  } catch (error) {
+    console.error('Request failed', error);
+    return null;
+  }
+  }
+
   return (
     <div>
       <Hero title='Create an IDO'
@@ -938,14 +982,16 @@ export const Admin: React.FC = () => {
             <>
               <Uik.Button text="Close" onClick={() => setOpen(false)} />
               <Uik.Button
-                text="Check Transcations"
+                text="Check Transactions"
                 success
                 fill
-                onClick={() =>
+                onClick={async() =>{
+                  const getBlockHeight = await fetchBlockHeight();
                   window.open(
-                    `https://reefscan.com/extrinsic/${txHash}`,
+                    `https://reefscan.com/extrinsic/${txHash}-${getBlockHeight}`,
                     "_blank"
-                  )
+                  );
+                }
                 }
               />
             </>
